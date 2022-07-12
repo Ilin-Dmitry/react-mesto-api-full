6f221ed8a5dffe4a3b0def64f10d52912b710574
Dmitry Ilin <dm-ilin@mail.ru>
2022-07-12 06:24:50 +0400

    Добавить в бэк логирование запросов и ошибок

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,6 +18,8 @@ app.use(cookieParser());
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -38,11 +41,7 @@ app.use(auth);
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
-// app.use((req, res) => {
-//   const ERROR_NOT_FOUND = 404;
-//   Promise.reject(new Error('Путь не найден'))
-//     .catch(() => res.status(ERROR_NOT_FOUND).send({ message: 'Такого адреса не существует' }));
-// });
+app.use(errorLogger);
 
 app.use(() => {
   throw new NotFoundError('Такого адреса не существует');
