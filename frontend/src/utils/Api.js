dad380ab874537dbd4d4c78002f57cccb3ff8f0b
Dmitry Ilin <dm-ilin@mail.ru>
2022-07-16 19:00:28 +0400

    Создать env на фронте

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -97,20 +97,10 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'http://localhost:3001',
+  baseUrl: `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`,
   headers: {
-    authorization: '3a1a4a90-7af6-46c1-8e15-2024cad1ccce',
     'Content-Type': 'application/json'
   }
 });
 
 export default api;
-
-// Для обращения на исходный сервер
-// const api = new Api({
-//   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-39',
-//   headers: {
-//     authorization: '3a1a4a90-7af6-46c1-8e15-2024cad1ccce',
-//     'Content-Type': 'application/json'
-//   }
-// });
