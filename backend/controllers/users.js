65ef0768bd1a6afebb7cfe6a10ddd87359b294a6
Dmitry Ilin <dm-ilin@mail.ru>
2022-07-18 22:57:52 +0400

    Отключить evt на бэке

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -100,10 +100,10 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      // const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       console.log('NODE_ENV from login => ', NODE_ENV);
       console.log('JWT_SECRET from login => ', JWT_SECRET);
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
+      // const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
       res.cookie('token', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,15 +6,15 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   console.log('NODE_ENV from auth.js => ', NODE_ENV);
-      console.log('JWT_SECRET from auth.js => ', JWT_SECRET);
+  console.log('JWT_SECRET from auth.js => ', JWT_SECRET);
   const { token } = req.cookies;
   if (!token) {
     throw new UnauthorizedError('Необходима авторизация!');
   }
   let payload;
   try {
-    // payload = jwt.verify(token, 'some-secret-key');
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    payload = jwt.verify(token, 'some-secret-key');
+    // payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     next(errModule.handleError(err, res));
     throw new UnauthorizedError('Необходима авторизация!');
