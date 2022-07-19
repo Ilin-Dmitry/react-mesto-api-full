6216da749c3abde43907a94cade04e544357f9af
Dmitry Ilin <dm-ilin@mail.ru>
2022-07-19 20:37:15 +0400

    Исправить замечания ревью 1

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const handleCors = require('./middlewares/handleCors');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(handleCors);
