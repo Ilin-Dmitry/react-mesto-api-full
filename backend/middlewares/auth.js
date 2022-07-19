0c6cdf962409b265853b73aeac2dd0f9459161c3
Dmitry Ilin <dm-ilin@mail.ru>
2022-07-19 20:26:13 +0400

    Revert "Исправить замечания ревью 1"
    This reverts commit ded8afe4ca5d4361a1310a06d7ff0e91b3a73020.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -13,8 +13,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    // next(errModule.handleError(err, res));
-    console.log('Ошибка');
+    next(errModule.handleError(err, res));
     throw new UnauthorizedError('Необходима авторизация!');
   }
   req.user = payload;
